5f32214071960291acb5d4db88ee2c72835b95cf
Thong Nguyen <ducthong1995@gmail.com>
2019-12-13 23:40:35 -0800

    First Commit

---
+++ app/src/main/java/com/thongnguyen/photogallery/GalleryItem.java
@@ -0,0 +1,34 @@
+package com.thongnguyen.photogallery;
+
+public class GalleryItem {
+    private String mCaption, mId, mUrl;
+
+    public String getmCaption() {
+        return mCaption;
+    }
+
+    public void setmCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getmId() {
+        return mId;
+    }
+
+    public void setmId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+}
